d8b687863feed49293647e24499c4992b6c2fe3c
ccistao <gamingphakesipaithe@gmail.com>
2025-12-03 09:53:49 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -185,8 +185,6 @@ local function waitForGameActive()
 end
 
 -- ⚡ HÀM KIỂM TRA PC HỢP LỆ + CÒN HACK ĐƯỢC
-local getPCProgress -- forward declaration
-
 local function isHackablePC(pc)
     if not pc then return false end
 
